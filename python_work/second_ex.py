a619bbf37acb62ac6eac09a18e62acf330251b70
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2017-11-14 16:26:38 +0000

    Completed Second Exercise. NB Exercise sheet was out of date for parts 1 and 2git add second_ex.py

--- python_work/second_ex.py
+++ python_work/second_ex.py
@@ -4,10 +4,43 @@
 # Remeber to use 4-space indentations for loops !!
 #1. Creating While loops
 # Infinite While loop - break added for Sanity!
+print "Part One. \n"
 a = 1 
 while a > 0:
     a += 1
     print a
 
-    if a > 100: 
+    if a > 10: 
 	break
+
+# An Uncalled while loop
+
+b = 1.0
+while b < 1: 
+    b = (b + 1)/2
+    print b 
+
+else: 
+    print "Loop not Called"
+
+print "Part Two. \n"
+# Iterating over a list of items
+mylist = [23, "hi", 2.4e-10]
+count = 0 
+
+while count < 3:
+    item = mylist[count]
+    print item, mylist.index(item) 
+    count += 1
+
+print "Part Three. \n"
+# Trying out if statements
+
+name = "Lisa"
+if name == "Lisa":
+    print name, "plays Saxophone"
+elif name == "Bart":
+    print name, "rides a Skateboard"
+else:
+    print name,"Lives in Springfield"
+
