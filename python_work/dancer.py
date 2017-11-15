3746f1f64541e87ec8ee03d9cb3ac13da4dfe45b
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2017-11-15 16:32:10 +0000

    Exercise on Scripts and Libraries and how they interact complete (Includes Dancing)

---
+++ python_work/dancer.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+# Script for busting out some wicked moves (Scripts and Libraries exercise)
+
+import sys
+from dancing.dance import boogie
+
+moves = sys.argv[1:]
+
+boogie(moves)
