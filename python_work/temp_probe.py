d9b8b0735ca403e65e26ab610119787d3632ee3b
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2017-11-17 15:38:39 +0000

    Added start of writing my data to a netCDF file and some self defined functions

--- python_work/temp_probe.py
+++ python_work/temp_probe.py
@@ -6,6 +6,7 @@ import serial
 import datetime 
 import io # input output moduel
 from datetime import timedelta
+from csv import reader #csv is a module for handling csv files....
 ser = serial.Serial(port = '/dev/ttyUSB0',baudrate= 9600,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE)
 outfile = '/home/user01/my-isc-work/python_work/serial_temp.tsv'
 
@@ -36,3 +37,25 @@ with open(outfile,'a') as f : #appends to existing file
             ser.close()
 
 ser.close()
+
+
+def time_conv(t):
+    t = datetime.strptime(t, '%Y-%m-%dT%H:%M:%S.%f') # converts time saved in my .tsv file and returns a datetime obejct
+    return t
+
+def CtoK(T_C)
+    T_K = T_C + 273.15 #Converts temperatures in celsius to Kelvin 
+    return T_K
+
+infile = 'serial_temp.tsv'
+outfile = 'sensor_data.nc'
+
+#we will need to parse the data to python lists
+t = []
+temp = []
+#Open our tsv file and have a read
+with open(infile, 'rb') as tsv:
+    tsvreader = reader(tsv,delimeter = '\t')
+    for row in tsvreader:
+        t.append(time_conv(row[0])) #for each row in the file we read feed to first item (time) into my conversion funciton and append the resutl to the times list
+        temp.append(CtoK(row[1])) #for each row we read feed to second item (temp) into my conversion funciton and append the resutl to the temperature list
